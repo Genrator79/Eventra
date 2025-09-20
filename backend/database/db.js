9a06e2910d6853c8136c416b3ad85d06335d3cf3
Abhijeet Kumar <abhijeetkumar@Abhijeets-MacBook-Pro-3.local>
2025-09-20 18:32:59 +0530

    correcting backend

--- backend/database/db.js
+++ backend/database/db.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose")
 const ConnectToDB = async()=>{
     try{
         await mongoose.connect(process.env.MONGO_URI);
-        console.log("MongoDB connected successfully !")
+        console.log("MongoDB connected successfully !!!")
     }
     catch(err){
         console.log("MongoDB connection failed !!!", err);
@@ -11,3 +11,4 @@ const ConnectToDB = async()=>{
 }
 
 module.exports = ConnectToDB;
+
